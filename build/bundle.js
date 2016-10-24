91dc03615480802f79b1679363dc3f21b5faab88
Jason C <jasoncai528@gmail.com>
2016-10-24 14:57:42 -0700

    Able to draw a path

--- build/bundle.js
+++ build/bundle.js
@@ -50,7 +50,7 @@
 
 	$(".hello_text").html(game_state.sayHelloInEnglish());
 	$(".button").click(function(){
-	    game_state.clickFunc();               
+	    game_state.clickFunc();
 	});
 	/*var background = new Raster("img/bg1.jpg", [400, 300]);
 	background.position = view.center;*/
@@ -10298,6 +10298,23 @@
 	    clickFunc: function(){
 	    document.getElementById('bgimg').style.display = 'none';
 	    document.getElementById('lv1').style.display = 'block';
+			// Get a reference to the canvas object
+			var canvas = document.getElementById('myCanvas');
+			// Create an empty project and a view for the canvas:
+			paper.setup(canvas);
+			// Create a Paper.js Path to draw a line into it:
+			var path = new paper.Path();
+			// Give the stroke a color
+			path.strokeColor = 'black';
+			var start = new paper.Point(100, 100);
+			// Move to start and draw a line from there
+			path.moveTo(start);
+			// Note that the plus operator on Point objects does not work
+			// in JavaScript. Instead, we need to call the add() function:
+			path.lineTo(start.add([ 200, -50 ]));
+			// Draw the view now:
+			paper.view.draw();
+		
 	   }
 	};
 
--- src/game.js
+++ src/game.js
@@ -4,7 +4,7 @@ var game_state = require("./game_state.js");
 
 $(".hello_text").html(game_state.sayHelloInEnglish());
 $(".button").click(function(){
-    game_state.clickFunc();               
+    game_state.clickFunc();
 });
 /*var background = new Raster("img/bg1.jpg", [400, 300]);
 background.position = view.center;*/
