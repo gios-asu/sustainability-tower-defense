a1e8c74d4ae8d751332ccbde467857b7d3f8c9ec
Daniel Yo <dnl992004@yahoo.com.au>
2016-10-26 13:57:35 -0700

    added mouse events
    circle now follows mouse
    still need to do path following

--- build/bundle.js
+++ build/bundle.js
@@ -10300,25 +10300,13 @@
 	    document.getElementById('bgimg').style.display = 'none';
 	    document.getElementById('lv1').style.display = 'block';
 			// Get a reference to the canvas object
-			var canvas = document.getElementById('myCanvas');
-			// Create an empty project and a view for the canvas:
-			paper.setup(canvas);
-			// Create a Paper.js Path to draw a line into it:
-			var path = new paper.Path();
-	        
-			// Give the stroke a color
-			path.strokeColor = 'black';
-			var start = new paper.Point(0, 50);
-			// Move to start and draw a line from there
-			path.moveTo(start);
-			// Note that the plus operator on Point objects does not work
-			// in JavaScript. Instead, we need to call the add() function:
-			path.lineTo(start.add([ 200, 50 ]));
-	        
-	        makeCircle();//from function AI
-	        
+	        paper.install(window);//needed to do animiation
+			//var canvas = document.getElementById('myCanvas');
+			paper.setup("myCanvas");
+	        main();
 			// Draw the view now:
-			paper.view.draw();
+	    
+			//paper.view.draw();
 		
 	   }
 
--- src/AI.js
+++ src/AI.js
@@ -1,8 +1,50 @@
+var square, circle;
+
+function path1()
+{
+    // Create a Paper.js Path to draw a line into it:
+		var line = new paper.Path();
+        
+		// Give the stroke a color
+		line.strokeColor = 'black';
+		var start = new paper.Point(0, 200);
+		// Move to start and draw a line from there
+		line.moveTo(start);
+		// Note that the plus operator on Point objects does not work
+		// in JavaScript. Instead, we need to call the add() function:
+		line.lineTo([ 700, 200 ]);
+}
+
 function makeCircle()
 {
-    var shape = new paper.Shape.Circle({
-            center: [80, 50],
-            radius: 30,
-            strokeColor: 'black'
+    circle = new Path.Circle({
+            center: [75, 75],
+	        radius: 70,
+	       fillColor: 'red'
         });
+}
+
+function makeRect()
+{
+    square = new Path.Rectangle([75, 75], [100, 100]);
+		square.strokeColor = 'black';
+    
+   
+}
+function main()//everything goes here
+{
+    path1();
+    makeRect();
+    makeCircle();
+     view.onFrame = function(event) {
+			// On each frame, rotate the path by 3 degrees:
+			square.rotate(3);
+         circle.fillColor.hue+=1;
+            
+		}
+
+    circle.onMouseMove = function(event) {//follow mouse code
+    this.position = event.point;
+    }
+  
 }
