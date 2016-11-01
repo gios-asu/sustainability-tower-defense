483cc9e88f6d5a9467597836582ba25321b10940
Jason C <jasoncai528@gmail.com>
2016-11-01 13:42:02 -0700

    place the turret function in a separate file

--- src/AI.js
+++ src/AI.js
@@ -64,22 +64,4 @@ function main()//everything goes here
   followPath(); 
 }
 
-function placeTurret(e){
-    var circle = new Path.Circle({
-        center: [event.clientX, event.clientY],
-	    radius: 50,
-	    fillColor: 'red'
-    });
-  
-    circle.onMouseMove = function(event) {//follow mouse code
-        this.position = event.point;
-    }
-    circle.onMouseDown = function(event){
-        turret = new Path.Circle({
-            center : [event.point.x, event.point.y],
-            radius : 20,
-            fillColor : 'blue'
-        });
-       
-    }   
-}
+
