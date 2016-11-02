f486fe972de2928fece3bb9022bcc5500ea45363
Daniel Yo <dnl992004@yahoo.com.au>
2016-11-02 13:09:14 -0700

    fixed spacing
    some of them

--- src/AI.js
+++ src/AI.js
@@ -11,7 +11,7 @@ function path1() {
   // Give the stroke a color
   line.strokeColor = 'black';
   start = new paper.Point(startx[0], starty[0]);
-		// Move to start and draw a line from there
+  // Move to start and draw a line from there
   line.moveTo(start);
   // Note that the plus operator on Point objects does not work
   // in JavaScript. Instead, we need to call the add() function:
@@ -63,82 +63,74 @@ function makeCircle(start, end) {
 }
 
 ////AI path following
-function followPath(obj, st){//(moving obj, st holds array index value)
+function followPath(obj, st) {//(moving obj, st holds array index value)
 
   var steps = 200;//speed
   var dx = 0, dy=0;
   
-  if (endx[st] - startx[st] != 0 && endy[st] -starty[st] == 0)
-    {
+  if (endx[st] - startx[st] != 0 && endy[st] -starty[st] == 0) {
        //move horizontal
       dx = (endx[st] - obj.position.x) / steps; //endpoint - obj position
       view.onFrame = function(event) {    
         // do the movement
-        if(obj.position.x != endx[st])
-          obj.position.x += dx;
-        else
-          {
-            var check = st + 1;
-            if (check <= startx.length-1)
-            {//waits until animation comes to a stop to recursively call
-              st++;
-              followPath(obj,st);
-            }
+        if(obj.position.x != endx[st]){
+           obj.position.x += dx;
+        }
+         
+        else {
+          var check = st + 1;
+          if (check <= startx.length-1) {
+            //waits until animation comes to a stop to recursively call
+            st++;
+            followPath(obj,st);
           }
-           
+        }     
       }
-    }
+  }
   //move vertical
-  else if(endy[st]-starty[st] != 0 && endx[st] - startx[st] == 0)
-    {
+  else if(endy[st] - starty[st] != 0 && endx[st] - startx[st] == 0) {
         //move vert
       dy = (endy[st] - obj.position.y)/steps; //endpoint - obj position
       view.onFrame = function(event) {    
           // do the movement
-        if(obj.position.y != endy[st])
+        if(obj.position.y != endy[st]) {
           obj.position.y += dy;
-        else
-          {
+        }
+          
+        else {
             var check = st+1;
-            if(check <=startx.length-1)
-            {//waits until animation comes to a stop to recursively call
+            if(check <=startx.length-1) {
+              //waits until animation comes to a stop to recursively call
               st++;
               followPath(obj,st);
             }
           }   
       }
     }
-  else//move diagonal
-    {
-      dx = (endx[st] - obj.position.x)/steps; //endpoint - obj position
-      dy = (endy[st] - obj.position.y)/steps; //endpoint - obj position
-      view.onFrame = function(event) {    
+  else {//move diagonal
+    dx = (endx[st] - obj.position.x)/steps; //endpoint - obj position
+    dy = (endy[st] - obj.position.y)/steps; //endpoint - obj position
+    view.onFrame = function(event) {    
           // do the movement
-        if(obj.position.y != endy[st])
-          {
-            obj.position.y += dy;
-            obj.position.x += dx;
-          }
-        else
-          {
-            var check = st+1;
-            if(check <=startx.length-1)
-            {//waits until animation comes to a stop to recursively call
-              st++;
-              followPath(obj,st);
-            }
-            else
-              {
-                //send some msg let system know an enemy breached the gates
-              }
-          }   
+      if(obj.position.y != endy[st]) {
+        obj.position.y += dy;
+        obj.position.x += dx;
       }
+      else {
+        var check = st+1;
+        if(check <=startx.length-1) {//waits until animation comes to a stop to recursively call
+          st++;
+          followPath(obj,st);
+        }
+        else {
+                //send some msg let system know an enemy breached the gates
+        }
+      }   
     }
-  
+  } 
 }
 
-function main()//everything goes here
-{
+function main() {//everything goes here
   path1();//draw path
   makeCircle(startx[0],starty[0]);//make obj
   followPath(enemy,0);//move obj
