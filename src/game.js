912d29547cbf911d91e0c199f1f19b90e358edd6
Daniel Yo <dnl992004@yahoo.com.au>
2017-01-30 14:33:44 -0700

    Added buttons/dropdown
    more main menu buttons.
    need help on drop down…

--- src/game.js
+++ src/game.js
@@ -2,16 +2,29 @@ var $ = require("jquery");
 var game_state = require("./game_state.js");
 
 //$.getScript("./AI.js");
-$(".button").click(function() {
+$("#buttonStart").click(function() {
   game_state.clickFunc();
 });
 
+$("#buttonInfo").click(function() {//change background to info background
+  //document.getElementById("bgimg").style.backgroundImage = "url('../img/bgInfo.jpg')";
+  //$(".button").hide();
+  document.getElementById("bgimg").style.display = "none";
+  document.getElementById("infoPage").style.display = "block";
+});
 
 
+$("#buttonExtra").click(function() {//change background to other background
+  
+});
 
+$("#back").click(function() {//go back to main menu
+  document.getElementById("bgimg").style.display = "block";
+  document.getElementById("infoPage").style.display = "none";
+});
 
-
-
-
+/*$("#myDropdown").click(function() {
+  document.getElementById("myDropdown").classList.toggle("show");
+});*/
 /*var background = new Raster("img/bg1.jpg", [400, 300]);
 background.position = view.center;*/
