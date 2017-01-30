7f78315dd7934f8015ff4ea2f6b0857b08ac2e18
Daniel Yo <dnl992004@yahoo.com.au>
2017-01-30 16:01:44 -0700

    drop down

--- build/bundle.js
+++ build/bundle.js
@@ -64,16 +64,15 @@
 	  
 	});
 
-	$("#back").click(function() {//go back to main menu
+
+	$("#backHome").click(function() {
 	  document.getElementById("bgimg").style.display = "block";
 	  document.getElementById("infoPage").style.display = "none";
 	});
 
-	/*$("#myDropdown").click(function() {
-	  document.getElementById("myDropdown").classList.toggle("show");
-	});*/
-	/*var background = new Raster("img/bg1.jpg", [400, 300]);
-	background.position = view.center;*/
+	$("#dropB").click(function() {//ignore this function..
+	 document.getElementById("myDropdown").classList.toggle("show");
+	});
 
 
 /***/ },
